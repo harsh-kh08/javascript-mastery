43ad42dbf40e25a3ef47124fcedec661e90cb4e6
Harsh Khandelwal <85062651+harsh-kh08@users.noreply.github.com>
2024-06-10 10:37:54 +0000

    Added dataTypes

---
+++ 1_Basics/2_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // This line of code will tell compiler to treat all this javascript code as new js version.
+
+/*
+
+- Do not use alert( 3 + 3) because we are using nodejs, not browser.
+- Null is a representation of empty values. It is a standalone value
+- Undefined value of a variabe occurs when we define a variable and do not assign value to it.
+- Remember :
+=> Data Type of null is object (dataType  null).
+=> Data Type of undefined is undefined (dataType undefined).
+
+*/
+
+// Example Code =>
+
+let name = "JavaScript"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
